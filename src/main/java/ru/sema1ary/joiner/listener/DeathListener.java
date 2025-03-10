356571ab8d29d465a7f3ebf023913a28f5943340
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-10 17:20:45 +0300

    Death message disabler

---
+++ src/main/java/ru/sema1ary/joiner/listener/DeathListener.java
@@ -0,0 +1,12 @@
+package ru.sema1ary.joiner.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        event.deathMessage(null);
+    }
+}
